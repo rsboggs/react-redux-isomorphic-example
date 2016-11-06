93389bbfc4cdff698154597111da1ef06e3f11df
Robert Boggs <robertstephenboggs@gmail.com>
2016-11-05 21:35:25 -0700

    Update webpack to minimize build size

--- webpack.config.js
+++ webpack.config.js
@@ -17,56 +17,57 @@ var plugins = [
 
 var config = {
 
-  devtool: '#inline-source-map',
+    devtool: 'cheap-module-source-map',
 
-  entry: entries,
+    entry: entries,
 
-  output: {
-	path: path.join(__dirname, 'build'),
-	filename: 'bundle.js',
-	publicPath: '/static/'
+    output: {
+
+    path: path.join(__dirname, 'build'),
+
+    filename: 'bundle.js',
+
+    publicPath: '/static/'
+  
   },
 
-  //
   plugins: plugins,
 
-  //
   resolve: {
-	alias: {
-	},
-	// require() file without adding .jsx and .js .suffix
-	extensions: ['', '.js', '.jsx']
+
+  	alias: {
+  	},
+  	// require() file without adding .jsx and .js .suffix
+  	extensions: ['', '.js', '.jsx']
   },
 
   // be sure to add 'stage-0' in .babelrc to support es7 syntax
   module: {
-	loaders: [
-		{
-		  test: /\.jsx?$/,
-		  loader: 'babel',
-		  exclude: /node_modules/,
-		  include: __dirname,
-		  query: {
-		    presets: ["es2015", "stage-0", "react" ],
-		    plugins: [ "transform-decorators-legacy" ],
-		  }
-		},
-		{
-		  test: /\.css$/,
-		  loader: "style!css",
-		},
-	]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        include: __dirname,
+        query: {
+          presets: ["es2015", "stage-0", "react" ],
+          plugins: [ "transform-decorators-legacy" ],
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style!css",
+      },
+    ]
   }
 };
 
-// Hot mode
-if (process.env.NODE_ENV === 'development') {
-  config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
-}
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  config.plugins.unshift(new webpack.optimize.OccurrenceOrderPlugin());
+  config.plugins.unshift(new webpack.optimize.UglifyJsPlugin());
+} else {
+  config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
 }
 
 module.exports = config;
